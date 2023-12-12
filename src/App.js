81ce20ad8fce3e5fc5f611604f9e368ba51f9005
Hritika Phapale <hritikaphapale072@gmail.com>
2023-12-12 12:31:04 +0530

    Adding heading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import your CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faUndo } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+const App = () => {
+  const [initialMinutes, setInitialMinutes] = useState(0);
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [totalSeconds, setTotalSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [remainingSeconds, setRemainingSeconds] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning && totalSeconds > 0) {
+      interval = setInterval(() => {
+        setTotalSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+    } else if (totalSeconds === 0) {
+      clearInterval(interval);
+      setIsRunning(false);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning, totalSeconds]);
+
+  const handleStart = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      setTotalSeconds((prevSeconds) => prevSeconds || inputMinutes * 60);
+    }
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTotalSeconds(0);
+    setInputMinutes(0);
+    setRemainingSeconds(0);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+    setRemainingSeconds(totalSeconds);
+  };
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setInputMinutes(parseInt(value, 10));
+  };
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="countdown-container">
+      <h1>Countdown Timer</h1>
+      <div>
+        <label htmlFor="minutes">Minutes: </label>
+        <input
+          type="number"
+          id="minutes"
+          value={isRunning ? Math.floor(totalSeconds / 60) : inputMinutes}
+          onChange={handleChange}
+          disabled={isRunning}
+        />
+      </div>
+      <div>
+        <p>Time Remaining: {formatTime(isRunning ? totalSeconds : remainingSeconds)}</p>
+      </div>
+      <div>
+        {isRunning ? (
+          <button onClick={handlePause}>
+            <FontAwesomeIcon icon={faPause} />
+          </button>
+        ) : (
+          <button onClick={handleStart}>
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        )}
+        <button onClick={handleReset}>
+          <FontAwesomeIcon icon={faUndo} />
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
